d35e947d8dd948772f4e4868fe3ecb9faf651e3f
frdrk00 <masterkooo@gmail.com>
2023-05-05 21:44:57 +0200

    Laying the app foundation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import Button from '../../components/ui/Button'
+
+interface pageProps {}
+
+const page: FC<pageProps> = () => {
+  return <Button  >hello world</Button>
+}
+
+export default page
