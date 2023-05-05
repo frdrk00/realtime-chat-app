1092550c5a84d2a8dea0e18236efdef1c8a70769
frdrk00 <masterkooo@gmail.com>
2023-05-05 21:58:48 +0200

    Integrating our database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
+import { db } from "@/lib/db";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <div className="text-red-500">hello world</div>;
+export default async function Home() {
+  await db.set("hello", "hello");
+  
+  return <div className="text-red-500">hallo</div>;
 }
---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import { Redis } from "@upstash/redis";
+
+const url = process.env.UPSTASH_REDIS_REST_URL
+const token = process.env.UPSTASH_REDIS_REST_TOKEN
+
+export const db = new Redis({
+    /* @ts-ignore */
+    url: url,
+    token: token,
+})
