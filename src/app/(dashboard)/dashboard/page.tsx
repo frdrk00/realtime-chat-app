92d221f4679b5c19d22658adfda76c721cb49234
frdrk00 <masterkooo@gmail.com>
2023-05-06 18:10:26 +0200

    Protecting our sensitive routes

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -6,7 +6,7 @@ const page = async () => {
 
   const session = await getServerSession(authOptions)
   
-  return <pre>{JSON.stringify(session)}</pre>
+  return <div>Dash</div>
 }
 
 export default page
